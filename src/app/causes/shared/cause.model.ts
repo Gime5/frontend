bb5db332e2c316751dc0ef8532c5a7601cf0a24b
Luis Mas <lmas@it2b.es>
2017-02-05 19:26:33 +0100

    Cause Improvements

--- src/app/causes/shared/cause.model.ts
+++ src/app/causes/shared/cause.model.ts
@@ -1,19 +1,23 @@
 export interface ICause {
   id: number;
   name: string;
+  content: string;
   remaining: number;
   total: number;
   segment: string;
   isPrivate: boolean;
+  rel_province: number;
 }
 
 export class Cause implements ICause {
     constructor(public id: number,
         public name: string,
+        public content: string,
         public remaining: number,
         public total: number,
         public segment: string,
-        public isPrivate: boolean) {
+        public isPrivate: boolean,
+        public rel_province: number) {
 
         }
 }
